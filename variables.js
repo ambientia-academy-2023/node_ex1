8d6f314c5f08224ed558857120950c7e6d87a75e
Pekka Alaluukas <pekka.alaluukas@hotmail.com>
2023-01-20 09:52:36 +0200

    muuttuja esimerkki

---
+++ variables.js
@@ -0,0 +1,18 @@
+//var määrittää globaalin muuttujan
+var fname="Teppo";
+console.log("Nimi on "+fname);
+
+for(var x=1; x<5; x++){
+    console.log("x="+x);
+}
+console.log("silmukan jälkeen x="+x);
+
+//let määrittää local muuttujan
+for(let y=1; y<5; y++){
+    console.log("y="+y);
+}
+//y on näkyvillä vain silmukan sisällä
+console.log("silmukan jälkeen y="+y);
+
+//const on myös globaali, mutta sen arvoa ei voi alustamisen
+//jälkeen muuttaa
